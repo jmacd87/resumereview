60207b0bc9071264f5541b1ae13b087a47378cf4
jmacd87 <jmacd87@gmail.com>
2020-05-15 13:21:49 -0300

    heroku deploy

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,12 @@ const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
-
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('backend/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'backend', 'build', 'index.html'))
+    })
+}
 const uri = process.env.ATLAS_URI
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
 
