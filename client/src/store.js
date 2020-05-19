0c2d38c89e79225bc0afe2dff8313b03a68be523
jmacd87 <jmacd87@gmail.com>
2020-05-19 12:06:53 -0300

    remove redux tools

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
@@ -6,11 +6,12 @@ const initialState = {};
 
 const middleWare = [thunk];
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
     rootReducer,
     initialState,
-    composeEnhancers(applyMiddleware(...middleWare))
+    applyMiddleware(...middleWare)
+    // composeEnhancers(applyMiddleware(...middleWare))
 );
 
 export default store;
