7ff93b435fc3917e74ec93136746e3fd525ad996
jmacd87 <jmacd87@gmail.com>
2020-05-20 18:21:01 -0300

    server.js

--- server.js
+++ server.js
@@ -10,14 +10,6 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-//serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-    //set static folder
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-    })
-}
 const uri = process.env.ATLAS_URI || 5000
 mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
 
@@ -26,7 +18,14 @@ const connection = mongoose.connection
 connection.once('open', () => {
     console.log("MongoDB connection established")
 })
-
+//serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 const recipesRouter = require('./routes/recipes')
 const usersRouter = require('./routes/users')
 
